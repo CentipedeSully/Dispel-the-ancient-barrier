ec9bd1869ce0a25e3f24886c142272beba0884f0
Will Smith <sullivansmith057@gmail.com>
2024-02-21 15:37:10 -0600

    Created hookups to the UI bar displays. Started implementing a fade effect for the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,44 @@ public class GameManager: MonoBehaviour
     //Declarations
     [SerializeField] private InputReader _inputReader;
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private GameObject _healthBar;
+    [SerializeField] private GameObject _energyBar;
+    [SerializeField] private GameObject _staminaBar;
+    [SerializeField] private GameObject _healthDisplay;
+    [SerializeField] private GameObject _energyDisplay;
+    [SerializeField] private GameObject _staminaDisplay;
 
 
     //Internal Utils
+    private void SetBar(float newPercentage, GameObject barObject)
+    {
+        //ignore the command if the bar doesn't exist
+        if (barObject == null)
+            return;
 
+        //Clamp the value
+        newPercentage = Mathf.Clamp(newPercentage, 0, 1.0f);
+
+        //Round the value to the nearest hundreth
+        newPercentage = ((int)(newPercentage * 100)) / 100f;
+
+        //Update the bar object
+        Vector3 newScale = new Vector3(newPercentage, barObject.transform.localScale.y, barObject.transform.localScale.z);
+        barObject.transform.localScale = newScale;
+
+    }
+
+    private void ShowDisplay(GameObject display)
+    {
+        if (display != null)
+            display.SetActive(true);
+    }
+
+    private void HideDisplay(GameObject display)
+    {
+        if (display != null)
+            display.SetActive(false);
+    }
 
 
 
@@ -25,7 +59,52 @@ public class GameManager: MonoBehaviour
         return _playerController;
     }
 
+    public void UpdateHealthBar(float newRelativePercentage)
+    {
+        SetBar(newRelativePercentage, _healthBar);
+    }
 
+    public void UpdateEnergyBar(float newRelativePercentage)
+    {
+        SetBar(newRelativePercentage, _energyBar);
+    }
+
+    public void UpdateStaminaBar(float newRelativePercentage)
+    {
+        SetBar(newRelativePercentage, _staminaBar);
+    }
+
+    public void ShowAllDisplays()
+    {
+        ShowDisplay(_healthDisplay);
+        ShowDisplay(_staminaDisplay);
+        ShowDisplay(_energyDisplay);
+    }
+
+    public void HideAllDisplays()
+    {
+        HideDisplay(_healthDisplay);
+        HideDisplay(_staminaDisplay);
+        HideDisplay(_energyDisplay);
+    }
+
+    public void ShowHealth()
+    {
+        ShowDisplay(_healthDisplay);
+    }
+
+    public void ShowEnergy()
+    {
+        ShowDisplay(_energyDisplay);
+    }
+
+    public void ShowStamina()
+    {
+        ShowDisplay(_staminaDisplay);
+    }
+
+
+    
     //Debugging
 
 
